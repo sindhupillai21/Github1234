324d252e357c72e80cf9daaddb0015e162aca4b0
avijayan73 <88583248+avijayan73@users.noreply.github.com>
2021-08-07 08:30:15 -0400

    Add project files.

---
+++ XMLLoad/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(30, 111)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(718, 289)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(77, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(142, 40)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Select XML File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(504, 29)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(182, 40)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(292, 29)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(142, 40)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "CreateXMLFile"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ XMLLoad/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim ds As New DataSet
+
+        Dim i As Integer
+
+        Dim openFileDialog As OpenFileDialog = New OpenFileDialog()
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim xmlFilePath As String = openFileDialog.FileName
+            'ds As DataSet = New DataSet()
+            ds.ReadXml(xmlFilePath)
+            DataGridView1.DataSource = ds.Tables(0)
+            'If DataGridView1.DataSource <= 0 Then
+
+            'For i = 0 To ds.Tables(0).Rows.Count - 1
+
+            'View1.DataSource.Rows(i).Item("Id")
+            'DataGridView1.DataSource.Rows(i).Item("FullName")
+            'dView1.DataSource.Rows(i).Item("Country")
+
+
+            'Next
+            'End If
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+
+        Try
+            con.ConnectionString = "Persist Security Info=False;Integrated Security=true;Initial Catalog=sql0329;server=DIT-181536L\SQLEXPRESS"
+            con.Open()
+            cmd.Connection = con
+
+            cmd = New SqlCommand("insert into customer (Id,Name,Country) values (@Id, @FullName,@Country)", con)
+            'cmd.ExecuteNonQuery()
+
+            For Each row As DataGridViewRow In DataGridView1.Rows
+                If Not row.IsNewRow Then
+                    cmd.Parameters("@Id").Value = row.Cells(0).Value.ToString
+                    cmd.Parameters("@FullName").Value = row.Cells(1).ToString
+                    cmd.Parameters("@Country").Value = row.Cells(2).ToString
+                End If
+            Next
+            cmd.ExecuteNonQuery()
+
+            MessageBox.Show("Data Inserted")
+
+
+            'MessageBox.Show("Data Inserted succeessfully")
+
+        Catch ex As Exception
+
+            MessageBox.Show("Invalid" & ex.Message)
+        Finally
+            con.Close()
+        End Try
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim ds As New DataSet
+
+        DataGridView1.DataSource.WriteXml("C:\Ajith-DIT\MyData2.xml", XmlWriteMode.WriteSchema)
+
+    End Sub
+End Class
