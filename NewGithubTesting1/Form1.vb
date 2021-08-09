4c689ea9d03910e533924023c121a1293b6317ee
avijayan73 <88583248+avijayan73@users.noreply.github.com>
2021-08-09 17:49:46 -0400

    Added New Msg Box

--- NewGithubTesting1/Form1.vb
+++ NewGithubTesting1/Form1.vb
@@ -2,5 +2,6 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Good Day")
         MsgBox("Good Day 1")
+        MsgBox("Good Day 2")
     End Sub
 End Class
