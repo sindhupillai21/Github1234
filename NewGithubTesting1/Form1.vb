a774013aa892ed7de0478b3b6edbe731babb0484
sindhupillai21 <87189888+sindhupillai21@users.noreply.github.com>
2021-08-09 17:39:13 -0400

    added mssagbox

--- NewGithubTesting1/Form1.vb
+++ NewGithubTesting1/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Good Day")
+        MsgBox("Good Day 1")
     End Sub
 End Class
